71a192fd6d3035318058880bbb964c0a8106d374
Lukas Elsner <open@mindrunner.de>
2019-07-17 18:52:20 -0600

    bump truffle versions

--- truffle.js
+++ truffle.js
@@ -8,12 +8,13 @@ module.exports = {
         return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
       },
       network_id: '*',
-      gas: 9999999
+      gas: 4500000,
+      gasPrice: 10000000000,
     }
   },
   compilers: {
     solc: {
-      version: "^0.4.25"
+      version: "^0.5.10"
     }
   }
 };
